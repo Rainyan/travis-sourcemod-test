1b5d40db9f2859d5416fbe3dc5a74bada6c039ed
Rainyan <cyberiatexture@gmail.com>
2016-04-06 23:11:36 +0300

    asd

--- scripting/tests/test_compile.sh
+++ scripting/tests/test_compile.sh
@@ -1,9 +1,6 @@
 # Script based on SMLIB's tests: https://github.com/bcserv/smlib/
 #!/bin/bash
 
-# Bash immediate exit and verbosity
-set -ev
-
 SMPATTERN="http:.*sourcemod-.*-linux.*"
 SMURL="http://www.sourcemod.net/smdrop/$SMVERSION/"
 SMPACKAGE=`lynx -dump "$SMURL" | egrep -o "$SMPATTERN" | tail -1`
@@ -14,19 +11,6 @@ tar -xzf $(basename "$SMPACKAGE")
 cp -R scripting/ addons/sourcemod/
 cd addons/sourcemod/scripting/
 chmod +x spcomp
-
-# Temporarily disable bash immediate exit
-set +e
-while read -r function; do
-	grep -Fq "$function" "$COMPILE_TARGET"
-    exit_code=$?
-
-    if [[ $exit_code != 0 ]]; then
-    	echo "[ERROR] Stock function \"$function\" not found in \"$COMPILE_TARGET\". Please add it."
-    	exit 1
-    fi
-done <<< "$functions"
-set -e
 
 ./spcomp -\; "$COMPILE_TARGET"
 
